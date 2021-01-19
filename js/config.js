10317a7473c47d98e8325f9bad4f990b497b6b35
Viktor Farkas <viktorfarkas@Viktors-MacBook-Air.local>
2021-01-19 14:44:02 +0100

    add text coordinates to config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,11 @@
 export default {
   numberOfLevels: 10,
   levelWidth: 1200,
-  levelheight: 575,
+  levelHeight: 675,
   groundPosition: 475,
   playerSpeed: 5,
+  startText: {
+    x: 265,
+    y: 320,
+  },
 }
